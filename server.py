8c12085f883ff645516f1346b5d4da449c81720d
vagrant <vagrant@vagrant.vm>
2017-01-22 23:20:52 +0000

    Added application response page and currency formatting

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import locale
+
+#Set location for currency formatting
+locale.setlocale(locale.LC_ALL, '')
 
 
 app = Flask(__name__)
@@ -9,18 +13,36 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
 @app.route("/")
 def serve_homepage():
     """Returns the homepage when users visit the website"""
+
     return render_template("index.html")
+
 
 @app.route("/application-form")
 def serve_application_form():
     """Displays the application for to the user"""
+
     job_list = ["Software Engineer", "QA Engineer", "Product Manager"]
 
     return render_template("application-form.html", job_list=job_list)
+
+
+@app.route("/application-success", methods=["POST"])
+def serve_application_response():
+    """Displpays the user's inputs as a response to their post request"""
+
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    salary = float(request.form.get("salary"))
+    job_title = request.form.get("job")
+
+    #Format salary
+    salary = locale.currency(salary, grouping=True)
+
+    return render_template("application-response.html", fname=fname, lname=lname,
+                           salary=salary, job_title=job_title)
 
 
 if __name__ == "__main__":
